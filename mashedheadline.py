707fd4091ab402f685f97212a36680c02f3ebe50
dsslater <dsslater@stanford.edu>
2015-11-14 23:49:42 -0800

    Update mashedheadline.py

--- mashedheadline.py
+++ mashedheadline.py
@@ -3,6 +3,10 @@ import top_stories
 import nl_substitutions
 import random
 
+queries1 = ['', 'news', 'politics', 'congress', 'election', 'trump', 'iowa', 'bush', 'clinton', 'rubio', 'media', 'poll']
+queries2 = ['movie', 'music', 'science', 'sports', 'space', 'award', 'movement', 'entertainment', 'hollywood', 'markets', 'animals', 'economy']
+
+#tweepy implementation
 class TwitterAPI:
     def __init__(self):
         consumer_key = ""
@@ -12,13 +16,16 @@ class TwitterAPI:
         access_token_secret = ""
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
-
     def tweet(self, message):
         self.api.update_status(status=message)
 
-queries1 = ['', 'news', 'politics', 'congress', 'election', 'trump', 'iowa', 'bush', 'clinton', 'rubio', 'media', 'poll']
-queries2 = ['movie', 'music', 'science', 'sports', 'space', 'award', 'movement', 'entertainment', 'hollywood', 'markets', 'animals', 'economy']
-
+"""
+Randomly chooses a political and then a soft news story. 
+These two stories are then passed to new_headline where 
+there is an attempt to 'mash' them. If this is successful 
+the new headline in tweeted. Otherwise a new combination 
+is tried.
+"""
 if __name__ == "__main__":
     twitter = TwitterAPI()
     tweet = False
@@ -26,5 +33,5 @@ if __name__ == "__main__":
         politics_story = top_stories.get_story(queries1[random.randint(0, len(queries1)-1)])
         soft_story = top_stories.get_story(queries2[random.randint(0, len(queries2)-1)])
         if politics_story != None and soft_story != None:
-            tweet = nltk_test.new_headline(soft_story[0], politics_story[0])
+            tweet = nl_substitutions.new_headline(soft_story[0], politics_story[0])
     twitter.tweet(tweet)
